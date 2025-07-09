4c30c671c0b3243c2a092d7f33250b643b344f37
aswinvrpai <aswin.199.vr@gmail.com>
2025-07-09 12:02:06 +0900

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from src.pipelines.prediction_pipeline import CustomData, Prediction
+
+from flask import Flask, request, render_template, jsonify
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("page.html")
+
+@app.route('/price_predictor',methods=['POST'])
+def predict_operation():
+    if (request.method == 'POST'):
+        
+        data = CustomData(
+            carat=request.form['carat'],
+            depth=request.form['depth'],
+            table=request.form['table'],
+            x=request.form['x'],
+            y=request.form['y'],
+            z=request.form['z'],
+            cut=request.form['cut'],
+            color=request.form['color'],
+            clarity=request.form['clarity']
+        )
+        new_data = data.get_data_as_dataframe()
+        
+        # Prediction Pipeline;
+        predict_pipleline = Prediction()
+        predict_result=predict_pipleline.predict(new_data)
+        predict_result = round(predict_result[0],2)
+        return render_template('page.html',result=predict_result)
+    
+if  __name__ == '__main__':
+        app.run(debug=True,port=8000)
